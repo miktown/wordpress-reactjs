790c535f24e8a82533bc686c836b9e923f691fbd
miktown <mcacho@mimotic.com>
2017-02-08 18:04:41 +0100

    fix filtesrs styles

--- app/components/content/informe/infoview/colegios.jsx
+++ app/components/content/informe/infoview/colegios.jsx
@@ -24,7 +24,7 @@ class InformeColegios extends React.Component {
   }
 
   stringTipoClean (tipo) {
-    return tipo.replace("col_", "")
+    return tipo.replace('col_', '').replace('u', 'ú')
   }
 
   isZonaChild (zonaSelected, zonaTargetId) {
@@ -55,7 +55,7 @@ class InformeColegios extends React.Component {
     .filter(colegio => this.state.filters[colegio.tipo])
     .filter(colegio => this.isValidZona(colegio.zona.id))
     .map(colegio => {
-      colegiosOutput.push(<li key={colegio.id}>
+      colegiosOutput.push(<li style={{padding:'0.5em 0.5em 0.1em'}} key={colegio.id}>
         <p style={{marginBottom: '1.5em'}} className='colegioIcon leftGo'>
           <span style={{color: '#333', fontWeight: '800'}}><a target='_blank' href={colegio.url}>{colegio.nombre}</a></span> <strong className={colegio.tipo}>{this.stringTipoClean(colegio.tipo)}</strong>
           <span style={{float: 'right', marginRight: '1em'}}>de <span style={{color: '#333', fontWeight: '800'}}>{colegio.profesor}</span> ({colegio.zona.nombre})</span>
@@ -82,9 +82,9 @@ class InformeColegios extends React.Component {
       </p>
       <div className='filtersWrap'>
         <div className='filters'>
-          <span onClick={this.onClickBtnFilter.bind(this, 'col_privado')} className={this.state.filters.draft ? 'selected' : null}>Privado</span>
-          <span onClick={this.onClickBtnFilter.bind(this, 'col_concertado')} className={this.state.filters.procesando ? 'selected' : null}>Concertado</span>
-          <span onClick={this.onClickBtnFilter.bind(this, 'col_publico')} className={this.state.filters.sin_stock ? 'selected' : null}>Público</span>
+          <span onClick={this.onClickBtnFilter.bind(this, 'col_privado')} className={this.state.filters.col_privado ? 'selected' : null}>Privado</span>
+          <span onClick={this.onClickBtnFilter.bind(this, 'col_concertado')} className={this.state.filters.col_concertado ? 'selected' : null}>Concertado</span>
+          <span onClick={this.onClickBtnFilter.bind(this, 'col_publico')} className={this.state.filters.col_publico ? 'selected' : null}>Público</span>
         </div>
       </div>
       <div>
