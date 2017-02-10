7d87088712fc3442f09bc5779549d834c6a2c838
miktown <mcacho@mimotic.com>
2017-02-10 14:12:18 +0100

    filtros informe totales

--- app/components/content/informe/infoview/totales.jsx
+++ app/components/content/informe/infoview/totales.jsx
@@ -41,6 +41,10 @@ class InformeTotales extends React.Component {
     return result
   }
 
+  filterColegiosChange (e) {
+    this.setState({colegioFilter: parseInt(e.target.value)})
+  }
+
   setFilterColegios () {
     let colegios = this.props.workData.alumnos.colegios
     let output = []
@@ -49,6 +53,11 @@ class InformeTotales extends React.Component {
     })
 
     return output
+  }
+
+  isInThisSchool (school) {
+    if (this.state.colegioFilter === 0) return true
+    return parseInt(school) === this.state.colegioFilter
   }
 
   decodeHtml (html) {
@@ -63,72 +72,14 @@ class InformeTotales extends React.Component {
     return moment(day, 'DD/MM/Y').isBetween(start, finish, 'days', '[]')
   }
 
-  getBajas (bajas) {
-    let mesesBajas = {}
-    let response = []
-
-    bajas.map(baja => {
-      if (baja !== '' && this.isDateBetween(baja)) {
-        let mesYear = moment(baja, 'DD/MM/Y').format('MMMY')
-        if (mesesBajas[mesYear] === undefined) {
-          mesesBajas[mesYear] = {
-            total: 1,
-            name: mesYear
-          }
-        } else mesesBajas[mesYear].total++
-      }
-    })
-
-    Object.keys(mesesBajas).forEach(function (key) {
-      if (mesesBajas[key].total > 1) {
-        response.push(<li style={{boxShadow: 'none', margin: 0, padding: '0.2em 0 0 4em'}} key={mesesBajas[key].name}>en <strong>{mesesBajas[key].name}</strong> - ({mesesBajas[key].total} veces)</li>)
-      }
-    })
-
-    return response
-  }
-
-  setAlumnoCurso (curso) {
-    return !curso ? 'sin nivel asignado' : curso
-  }
-
-  listAlumnos () {
-    let alumnos = this.props.workData.alumnos.alumnos
-    let alumnosOutput = []
-
-    alumnos
-    .filter(alumno => this.isValidZona(alumno.zona.id))
-    .filter(alumno => {
-      if (this.state.colegioFilter === 0) return true
-      return parseInt(alumno.colegio.id) === this.state.colegioFilter
-    })
-    .map(alumno => {
-      let bajasAlumnos = this.getBajas(alumno.bajas)
-      if (bajasAlumnos.length > 0) {
-        alumnosOutput.push(<li style={{padding: '0.5em 0.5em 0.1em'}} key={alumno.id}>
-
-          <p style={{marginBottom: '1.5em'}} className='studentIcon leftGo'>
-            <span style={{color: '#333', fontWeight: '800'}}><a target='_blank' href={alumno.url}>{alumno.nombre}</a> ({alumno.colegio.nombre})</span>
-            <strong className={alumno.tipo}>{this.setAlumnoCurso(alumno.curso.nombre)}</strong>
-            <span style={{float: 'right', marginRight: '1em'}}>de <span style={{color: '#333', fontWeight: '800'}}>{alumno.profesor}</span> {alumno.zona.nombre}</span>
-          </p>
-          <ul style={{marginTop: '-1em', marginBottom: '1em'}}>{bajasAlumnos}</ul>
-
-        </li>)
-      }
-    })
-
-    return alumnosOutput
-  }
-
   createViewEdad (totales) {
     let response = []
 
     // edad
     Object.keys(totales.edad).forEach(function (key) {
-      if (key === 'total') response.push(<li style={{boxShadow: 'none', margin: 0, padding: '0.2em 0 0 4em'}} key={'total'}><strong> ∑ {totales.edad[key]}</strong></li>)
-      else if (!totales.edad[key].name) response.push(<li style={{boxShadow: 'none', margin: 0, padding: '0.2em 0 0 4em'}} key={'sinasignar'}><strong>Sin Asignar</strong> - ({totales.edad[key].total} en total)</li>)
-      else response.push(<li style={{boxShadow: 'none', margin: 0, padding: '0.2em 0 0 4em'}} key={totales.edad[key].id}>en <strong>{totales.edad[key].name}</strong> - ({totales.edad[key].total} en total)</li>)
+      if (key === 'total') response.push(<li style={{boxShadow: 'none', margin: '1em 0 0 0', padding: '1em 0 1em 4em'}} key={'total'}><strong> ∑ {totales.edad[key]}</strong></li>)
+      else if (!totales.edad[key].name) response.push(<li style={{boxShadow: 'none', margin: 0, padding: '.5em 0 .5em 4em'}} key={'sinasignar'}><strong>Sin Asignar</strong> - ({totales.edad[key].total} en total)</li>)
+      else response.push(<li style={{boxShadow: 'none', margin: 0, padding: '0.2em 0 0 4em'}} key={totales.edad[key].id}>en <strong>{totales.edad[key].name}</strong> hay {totales.edad[key].total} alumnos</li>)
     })
 
     return response
@@ -137,11 +88,38 @@ class InformeTotales extends React.Component {
   createViewColegio (totales) {
     let response = []
 
-    // edad
+    // colegios
     Object.keys(totales.colegio).forEach(function (key) {
-      if (key === 'total') response.push(<li style={{boxShadow: 'none', margin: 0, padding: '0.2em 0 0 4em'}} key={'total'}><strong> ∑ {totales.colegio[key]}</strong></li>)
+      if (key === 'total') response.push(<li style={{boxShadow: 'none', margin: '1em 0 0 0', padding: '1em 0 1em 4em'}} key={'total'}><strong> ∑ {totales.colegio[key]}</strong></li>)
       else if (!totales.colegio[key].name) response.push(<li style={{boxShadow: 'none', margin: 0, padding: '0.2em 0 0 4em'}} key={'sinasignar'}><strong>Sin Asignar</strong> - ({totales.colegio[key].total} en total)</li>)
-      else response.push(<li style={{boxShadow: 'none', margin: 0, padding: '0.2em 0 0 4em'}} key={totales.colegio[key].id}>en <strong>{totales.colegio[key].name} ({totales.colegio[key].zona})</strong> - ({totales.colegio[key].total} en total)</li>)
+      else response.push(<li style={{boxShadow: 'none', margin: 0, padding: '0.2em 0 0 4em'}} key={totales.colegio[key].id}><strong>{totales.colegio[key].name} ({totales.colegio[key].zona})</strong> -> {totales.colegio[key].total} alumnos</li>)
+    })
+
+    return response
+  }
+
+  createViewClases (totales) {
+    let response = []
+
+    // clases
+    Object.keys(totales.clase).forEach(function (key) {
+      if (key === 'total') response.push(<li style={{boxShadow: 'none', margin: 0, padding: '0.2em 0 0 4em'}} key={'total'}><strong> ∑ {totales.clase[key]}</strong></li>)
+      else if (!totales.clase[key].name) response.push(<li style={{boxShadow: 'none', margin: 0, padding: '0.2em 0 0 4em'}} key={'sinasignar'}><strong>Sin Asignar</strong> - ({totales.clase[key].total} en total)</li>)
+      else response.push(<li style={{boxShadow: 'none', margin: 0, padding: '0.2em 0 0 4em'}} key={totales.clase[key].id}>en <strong><a href={totales.clase[key].url} target="_blank">{totales.clase[key].name}</a> ({totales.clase[key].zona})</strong> - ({totales.clase[key].total} en total)</li>)
+    })
+
+    return response
+  }
+
+  createViewAsignatura (totales) {
+    let response = []
+    let self = this
+
+    // asignatura
+    Object.keys(totales.asignatura).forEach(function (key) {
+      if (key === 'total') response.push(<li style={{boxShadow: 'none', margin: 0, padding: '0.2em 0 0 4em'}} key={'total'}><strong> ∑ {totales.asignatura[key]}</strong></li>)
+      else if (!totales.asignatura[key].name) response.push(<li style={{boxShadow: 'none', margin: 0, padding: '0.2em 0 0 4em'}} key={'sinasignar'}><strong>Sin Asignar</strong> - ({totales.asignatura[key].total} en total)</li>)
+      else response.push(<li style={{boxShadow: 'none', margin: 0, padding: '0.2em 0 0 4em'}} key={totales.asignatura[key].id}><strong>{self.decodeHtml(totales.asignatura[key].name)}</strong> - {totales.asignatura[key].total} alumnos</li>)
     })
 
     return response
@@ -162,70 +140,82 @@ class InformeTotales extends React.Component {
     totales.asignatura = {}
 
     alumnos.map(alumno => {
-      // colegio
-      totales.colegio['total']++
-      if (totales.colegio[alumno.colegio.id] === undefined) {
-        totales.colegio[alumno.colegio.id] = {
-          total: 1,
-          name: alumno.colegio.nombre,
-          id: alumno.colegio.id,
-          zona: alumno.zona.nombre
-        }
-      } else totales.colegio[alumno.colegio.id].total++
+      if(this.isValidZona(alumno.zona.id) && this.isInThisSchool(alumno.colegio.id)){
+          // colegio
+          totales.colegio['total']++
+          if (totales.colegio[alumno.colegio.id] === undefined) {
+            totales.colegio[alumno.colegio.id] = {
+              total: 1,
+              name: alumno.colegio.nombre,
+              id: alumno.colegio.id,
+              zona: alumno.zona.nombre
+            }
+          } else totales.colegio[alumno.colegio.id].total++
 
-      // edad
-      totales.edad['total']++
-      if (totales.edad[alumno.curso.id] === undefined) {
-        totales.edad[alumno.curso.id] = {
-          total: 1,
-          name: alumno.curso.nombre,
-          id: alumno.curso.id
-        }
-      } else totales.edad[alumno.curso.id].total++
+          // edad
+          totales.edad['total']++
+          if (totales.edad[alumno.curso.id] === undefined) {
+            totales.edad[alumno.curso.id] = {
+              total: 1,
+              name: alumno.curso.nombre,
+              id: alumno.curso.id
+            }
+          } else totales.edad[alumno.curso.id].total++
 
-      alumno.clases.map(clase => {
-          // clase
-        if (totales.clase[clase.clase] === undefined) {
-          totales.clase[clase.clase] = {
-            total: 1,
-            name: clase.name,
-            id: clase.clase,
-            url: clase.url
-          }
-        } else totales.clase[clase.clase].total++
+          alumno.clases.map(clase => {
+              // clase
+            if (totales.clase[clase.clase] === undefined) {
+              totales.clase[clase.clase] = {
+                total: 1,
+                name: clase.name,
+                id: clase.clase,
+                url: clase.url,
+                zona: alumno.zona.nombre
+              }
+            } else totales.clase[clase.clase].total++
 
-          // asignatura
-        if (totales.asignatura[clase.asignatura] === undefined) {
-          totales.asignatura[clase.asignatura] = {
-            total: 1,
-            name: clase.asignaturaname,
-            id: clase.asignatura
-          }
-        } else totales.asignatura[clase.asignatura].total++
-      })
+              // asignatura
+            if (totales.asignatura[clase.asignatura] === undefined) {
+              totales.asignatura[clase.asignatura] = {
+                total: 1,
+                name: clase.asignaturaname,
+                id: clase.asignatura
+              }
+            } else totales.asignatura[clase.asignatura].total++
+          })
+      }
     })
     console.log('totales', totales)
     return totales
   }
-// <select onChange={this.filterColegiosChange.bind(this)}>
-          //   <option value='0' key={0}>Filtrar Colegio</option>
-          //   {this.setFilterColegios()}
-          // </select>
-          //
+
   render () {
     let dataTotales = this.getTotales()
     let edad = this.createViewEdad(dataTotales)
     let colegio = this.createViewColegio(dataTotales)
+    let clase = this.createViewClases(dataTotales)
+    let asignatura = this.createViewAsignatura(dataTotales)
     return <div>
       <p className='introduction'>
         {dataTotales.length} {dataTotales.length === 1 ? 'alumno' : 'alumnos'} en <strong>{this.props.zona}</strong>
       </p>
       <div className='filtersWrap'>
-        <div className='filters' />
+        <div className='filters'>
+          <select onChange={this.filterColegiosChange.bind(this)}>
+            <option value='0' key={0}>Filtrar Colegio</option>
+            {this.setFilterColegios()}
+          </select>
+        </div>
       </div>
       <div>
+        <h2>Edad</h2>
         <ul className='listaProfesores'>{edad}</ul>
+        <h2>Colegios</h2>
         <ul className='listaProfesores'>{colegio}</ul>
+        <h2>Clases</h2>
+        <ul className='listaProfesores'>{clase}</ul>
+        <h2>Asignaturas</h2>
+        <ul className='listaProfesores'>{asignatura}</ul>
       </div>
     </div>
   }
